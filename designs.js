1a4c80fb660c69984a43501a8af4d03afabace75
Staci Holmes-Segarra <stacihs@hotmail.com>
2018-04-09 22:10:16 -0500

    Created makeGrid()
    Added ability to create a grid and add color to the individual cells

--- designs.js
+++ designs.js
@@ -1,10 +1,31 @@
-// Select color input
-// Select size input
+$(document).ready(function() {
+  //When size is submitted by the user, call makeGrid()
+  $("form").submit(function(event) {
+    makeGrid();
+    event.preventDefault();
+  });
 
-// When size is submitted by the user, call makeGrid()
+  function makeGrid(row, column) {
+    row = $("#inputHeight").val();
+    column = $("#inputWidth").val();
+    //create row
+    for (var i = 0; i < row; i++) {
+      $("table").append("<tr></tr>");
+      //create column
+      for (var j = 0; j < column; j++) {
+        $("tr:last-of-type").append("<td></td>");
+        $("td").addClass("box");
+        //assign box color
+        $("td").click(function() {
+          var colorChoice = $("#colorPicker").val();
+          $(this).css("background-color", colorChoice);
+        });
 
-function makeGrid() {
+      }
+    }
+  };
 
-// Your code goes here!
 
-}
+
+
+});
